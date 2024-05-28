7411d0199662a2319c3c8fd530f69f9c6289a62c
smanotasa <santiago.manotasarroyave@bse.eu>
2024-05-28 21:12:46 +0200

    Updating

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,43 +1,43 @@
 import pandas as pd
 
 def load_soi():
-    soi = pd.read_csv('./data/interim/soi.csv')
+    soi = pd.read_csv('/data/interim/soi.csv')
     soi['date'] = pd.to_datetime(soi['date'])
     return soi
 
 def load_capacity():
-    capacity = pd.read_csv('./data/interim/d_netcapacity.csv')
+    capacity = pd.read_csv('/data/interim/d_netcapacity.csv')
     capacity['total_capacity'] = capacity.groupby('date')['capacity_kW'].transform('sum')
     capacity['share'] = capacity['capacity_kW'] / capacity['total_capacity']
     return capacity
 
 def load_askprice():
-    askprice = pd.read_csv('./data/interim/d_supplyprice.csv')
+    askprice = pd.read_csv('/data/interim/d_supplyprice.csv')
     askprice['date'] = pd.to_datetime(askprice['date'])
     return askprice
 
 def load_bidprice():
-    bidprice = pd.read_csv('./data/interim/h_stockprice.csv')
+    bidprice = pd.read_csv('/data/interim/h_stockprice.csv')
     bidprice['datetime'] = pd.to_datetime(bidprice['datetime'])
     return bidprice
 
 def load_resource():
-    return pd.read_csv('./data/interim/resource.csv')
+    return pd.read_csv('/data/interim/resource.csv')
 
 def load_demand():
-    return pd.read_csv('./data/interim/h_demand.csv')
+    return pd.read_csv('/data/interim/h_demand.csv')
 
 def load_supply():
-    return pd.read_csv('./data/interim/h_supply.csv')
+    return pd.read_csv('/data/interim/h_supply.csv')
 
 def load_price_fuel():
-    return pd.read_csv('./data/interim/d_pricexfuel.csv')
+    return pd.read_csv('/data/interim/d_pricexfuel.csv')
 
 def load_mcost():
-    return pd.read_csv('./data/interim/h_marginalcost.csv')
+    return pd.read_csv('/data/interim/h_marginalcost.csv')
 
 def load_generation():
-    return pd.read_csv('./data/interim/h_generation.csv')
+    return pd.read_csv('/data/interim/h_generation.csv')
 
 def data_loader(*args):
     # Map dataframe names to loading functions
